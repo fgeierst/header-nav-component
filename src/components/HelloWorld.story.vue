33bd6b3e6f736801324ff8689cc0fb662dccca02
Florian Geierstanger <florian.geierstanger@pluswerk.ag>
2023-03-01 19:01:23 +0100

    Add histoire as a component explorer

---
+++ src/components/HelloWorld.story.vue
@@ -0,0 +1,9 @@
+<script setup>
+import HelloWorld from './HelloWorld.vue'
+</script>
+
+<template>
+  <Story>
+		<HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </Story>
+</template>
